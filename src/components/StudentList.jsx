0a3cd9a0e29474cec84be5a9271890ffd5b243cf
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-06-05 22:21:01 -0700

    Inpro to React Components

---
+++ src/components/StudentList.jsx
@@ -0,0 +1,16 @@
+import './StudentList.css';
+
+const StudentList = () => {
+  return (
+    <section>
+      <h2>Student List</h2>
+      <ul>
+        <li>Student A</li>
+        <li>Student B</li>
+        <li>Student C</li>
+      </ul>
+    </section>
+  );
+};
+
+export default StudentList;
